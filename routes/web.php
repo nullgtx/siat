5626cb2007c1f1e512e23b9e128695890da24820
yankotinuj <yankotinu.23@gmail.com>
2019-05-10 20:04:31 +0700

    penambahan routes page

--- routes/web.php
+++ routes/web.php
@@ -13,35 +13,48 @@
 
 Route::get('/', function () {
     return view('auth.login');
-});
+})->name('halamanutama');
 
 Route::get('/pilihcabang', function () {
     return view('pilihcabang');
 });
 
+Route::get('/absensikaryawan', function () {
+    return view('absensikaryawan');
+})->name('absensikaryawan');
+
 Route::get('/dashboard/kasir', function () {
     return view('dashboardkasir');
 })->name('dashboardkasir');
+
+Route::get('/dashboard/kasir/stokbarang', function () {
+    return view('stokbarangkasir');
+})->name('stokbarangkasir');
 
 Route::get('/dashboard/kepala', function () {
     return view('dashboardkepala');
 })->name('dashboardkepala');
 
-Route::get('/dashboard/stokbarang', function () {
-    $role = Auth::user()->role;
-    return view('stokbarang',['role'=>$role]);
-})->name('stokbarang');
+Route::get('/dashboard/kepala/tambahbarang', function () {
+    return view('tambahbarang');
+})->name('tambahbarang');
+
+Route::get('/dashboard/kepala/editbarang', function () {
+    return view('editbarang');
+})->name('editbarang');
+
+Route::get('/dashboard/kepala/stokbarang', function () {
+    return view('stokbarangkepala');
+})->name('stokbarangkepala');
 
 Route::get('/dashboard/pemilik', function () {
     return view('dashboardpemilik');
 })->name('dashboardpemilik');
 
-Route::get('/dashboard/pemilik/tambahbarang', function () {
-    return view('tambahbarang');
-})->name('tambahbarang');
-Route::get('/dashboard/pemilik/editbarang', function () {
-    return view('editbarang');
-})->name('editbarang');
+Route::get('/dashboard/pemilik/stokbarang', function () {
+    return view('stokbarangpemilik');
+})->name('stokbarangpemilik');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
