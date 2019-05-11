8919e0636143ae5988b143e67f7a28178a52ae45
yankotinuj <yankotinu.23@gmail.com>
2019-05-11 11:53:16 +0700

    tambah menu nav Data Karyawan

--- resources/views/layouts/navkepala.blade.php
+++ resources/views/layouts/navkepala.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/dashboard/kepala') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -34,16 +34,16 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dashboardkepala') }}">Dashboard<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('stokbarangkepala') }}">Stok Barang<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Barang Masuk Keluar<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('datakaryawankepala') }}">Gaji Karyawan<span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="{{ route('datakaryawankepala') }}">Data Karyawan<span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gaji Karyawan<span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Pengaturan Akun<span class="sr-only">(current)</span></a>
