74da209861333e1e776476c9fc80285481959f80
yankotinuj <yankotinu.23@gmail.com>
2019-05-11 11:54:09 +0700

    hapus menu dashboard diganti  logo siat

--- resources/views/layouts/navpemilik.blade.php
+++ resources/views/layouts/navpemilik.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/dashboard/pemilik') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -33,9 +33,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('dashboardpemilik') }}">Dashboard<span class="sr-only">(current)</span></a>
-                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('datakaryawanpemilik') }}">Data Karyawan<span class="sr-only">(current)</span></a>
                         </li>
