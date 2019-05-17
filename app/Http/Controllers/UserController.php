32e82dc8cb4bb789fe468240d5e5ae774a400e8d
yankotinuj <yankotinu.23@gmail.com>
2019-05-17 15:55:34 +0700

    Tambah Controller User buat akun

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = DB::table('users')->get();
+
+        return view('pengaturanakun', ['users' => $users]);
+    }
+}
