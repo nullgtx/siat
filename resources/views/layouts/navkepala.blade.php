67fe7bc97a3479d975a89d6b71ee1b34adba0207
Ezekiel Pradipta <49933787+ezekielpradipta@users.noreply.github.com>
2019-05-19 15:04:14 +0700

    change toast ke sweetalert tambahbarang

--- resources/views/layouts/navkepala.blade.php
+++ resources/views/layouts/navkepala.blade.php
@@ -6,7 +6,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -22,7 +22,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel='stylesheet'>
 </head>
 <body>
     <div id="app">
