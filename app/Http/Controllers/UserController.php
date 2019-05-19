ecb4ecb98b44a602dc8dc4ca3b7c05b7b744f961
yankotinuj <yankotinu.23@gmail.com>
2019-05-19 23:49:40 +0700

    Nambah method buat ganti pass tapi gagal

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\User;
 
 class UserController extends Controller
 {
@@ -14,4 +15,37 @@ class UserController extends Controller
 
         return view('pengaturanakun', ['users' => $users]);
     }
+    /*
+    public function getUser()
+    {
+        $users = DB::table('users')->find('id');
+        if(count($users)>0){
+            return view('ubahpassword',['users'=>$users]);
+        }
+        else{
+            return redirect('/dashboard/kepala/pengaturanakun');
+        }
+    }
+    */
+    /* -------------------------------------------------------------------
+    public function changePassword(Request $request){
+        if (!(Hash::check($request->get('passwordlama'), Auth::user()->password))) {
+            // The passwords matches
+            return redirect()->back()->with("error","Your current password does not matches with the password you provided. Please try again.");
+        }
+        if(strcmp($request->get('passwordlama'), $request->get('passwordbaru')) == 0){
+            //Current password and new password are same
+            return redirect()->back()->with("error","New Password cannot be same as your current password. Please choose a different password.");
+        }
+        $validatedData = $request->validate([
+            'passwordlama' => 'required',
+            'passwordbaru' => 'required|string|min:6|confirmed',
+        ]);
+        //Change Password
+        $user = Auth::user();
+        $user->password = bcrypt($request->get('passwordbaru'));
+        $user->save();
+        return redirect()->back()->with("success","Password changed successfully !");
+    }
+    ---------------------------------------------------- */
 }
