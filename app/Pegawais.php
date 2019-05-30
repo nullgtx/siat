7fddee2bd26d4ccdc33b41b6edf14bcd58bd84c3
yankotinuj <yankotinu.23@gmail.com>
2019-05-30 16:03:38 +0700

    betulin model di pegawai

--- app/Pegawais.php
+++ app/Pegawais.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawais extends Model
 {
     protected $table = 'datapegawai';
-    protected $fillable = ['id','idkaryawan','namakaryawan','jeniskelamin','alamat','tanggallahir','role'];
-    //
+    protected $fillable = ['id','id_cabang','idkaryawan','namakaryawan','jeniskelamin','alamat','tanggallahir','role'];
+    public function cabang(){
+        return $this->hasOne('App\cabang','id_cabang','id_cabang');
+    }
 }
