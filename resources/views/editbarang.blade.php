7174d216d704275431a0fb6206d42e50f023f37e
yankotinuj <yankotinu.23@gmail.com>
2019-06-01 10:16:35 +0700

    fix halaman edit barang cuman belum bisa simpan

--- resources/views/editbarang.blade.php
+++ resources/views/editbarang.blade.php
@@ -4,13 +4,14 @@
     <h4>Edit Barang</h4>
     <div class="row">
       <div class="col-8">
-        <form id="editbarang" method="post" action="javascript:void(0)">
-        <input type="hidden" name="kodebarang" value="{{ ($dataobat) ? $dataobat['kodebarang'] : '' }}">
+        <form id="editbarang" method="POST" action="/dashboard/kepala/editbarang/updatebarang">
         {{ csrf_field() }}
+        <input type="hidden" name="idbarang" value="{{ ($dataobat) ? $dataobat['id'] : '' }}">
+        
           <div class="form-group row">
             <label for="example-text-input" class="col-3 col-form-label">Kode Barang</label>
             <div class="col-9">
-              <input class="form-control" type="text" placeholder="Masukkan kode barang" value="{{ ($dataobat) ? $dataobat['kodebarang'] : '' }}" id="kodebarang" required>
+              <input class="form-control" type="text" placeholder="Masukkan kode barang" value="{{ $dataobat->kodebarang }}" id="kodebarang" required>
             </div>
           </div>
 
@@ -18,7 +19,7 @@
             <label for="example-text-input" class="col-3 col-form-label">Jenis Barang</label>
             <div class="col-9">
               <select id="jenisbarang" class="form-control" required>
-                <option value="" selected disable>Pilih jenis barang</option>
+                <option value="{{ $dataobat->jenisbarang }}" selected>{{ $dataobat->jenisbarang }}</option>
                 <option value="Serbuk">Serbuk</option>
                 <option value="Tablet">Tablet</option>
                 <option value="Tablet Kunyah">Tablet Kunyah</option>
@@ -34,7 +35,7 @@
           <div class="form-group row">
             <label for="example-text-input" class="col-3 col-form-label">Keterangan</label>
             <div class="col-9">
-              <input class="form-control" type="text" placeholder="Masukkan keterangan atau nama barang" id="keteranganbarang" required>
+              <input class="form-control" type="text" placeholder="Masukkan keterangan atau nama barang" value="{{ $dataobat->keteranganbarang }}" id="keteranganbarang" required>
             </div>
           </div>
 
@@ -42,7 +43,7 @@
             <label for="example-text-input" class="col-3 col-form-label">Satuan</label>
             <div class="col-9">
               <select id="satuanbarang" class="form-control" required>
-                <option value="" selected disable>Pilih satuan barang</option>
+                <option value="{{ $dataobat->satuanbarang }}" selected>{{ $dataobat->satuanbarang }}</option>
                 <option value="Strip">Strip</option>
                 <option value="Botol">Botol</option>
                 <option value="Pack">Pack</option>
@@ -58,7 +59,7 @@
                 <div class="input-group-prepend">
                   <span class="input-group-text">Rp.</span>
                 </div>
-                  <input type="number" class="form-control" id="hargabarang" placeholder="Masukkan harga barang" required>
+                  <input type="number" class="form-control" id="hargabarang" placeholder="Masukkan harga barang" value="{{ $dataobat->hargabarang }}"required>
                 <div class="input-group-append">
                   <span class="input-group-text">,00</span>
                 </div>
@@ -69,21 +70,21 @@
           <div class="form-group row">
             <label for="example-text-input" class="col-3 col-form-label">Jumlah Barang</label>
             <div class="col-9">
-              <input class="form-control" type="number" placeholder="Masukkan jumlah dalam satuan angka" id="jumlahbarang" required>
+              <input class="form-control" type="number" placeholder="Masukkan jumlah dalam satuan angka" value="{{ $dataobat->jumlahbarang }}" id="jumlahbarang" required>
             </div>
           </div>
 
           <div class="form-group row">
             <label for="example-date-input" class="col-3 col-form-label">Tanggal Masuk Barang</label>
             <div class="col-9">
-              <input class="form-control" type="date" id="tanggalmasuk" required>
+              <input class="form-control" type="date" id="tanggalmasuk" value="{{ $dataobat->tanggalmasuk }}" required>
             </div>
           </div>
 
           <div class="form-group row">
             <label for="example-date-input" class="col-3 col-form-label">Expired Barang</label>
             <div class="col-9">
-              <input class="form-control" type="date" id="tanggalexpired" required>
+              <input class="form-control" type="date" id="tanggalexpired" value="{{ $dataobat->tanggalexpired }}" required>
             </div>
           </div>
 
