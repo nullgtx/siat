69b37254db4cf33ffc90c3194a494daf0123c4d3
yankotinuj <yankotinu.23@gmail.com>
2019-06-03 21:57:08 +0700

    tambah pilih cabang buat absensi (belum fix)

--- resources/views/absensikaryawan.blade.php
+++ resources/views/absensikaryawan.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <h3>Absensi Karyawan</h3>
-    <h4>Apotek XXX</h4>
+    <h4><b>Apotek Aja Mriyang</b></h4>
     <div class="row pt-4">
         <div class="col">
         <!-- Isi Kolom Kesatu -->
@@ -10,15 +10,23 @@
         <div class="col-6">
             <form>
                 <div class="form-group">
-                    <label for="idKaryawan">Masukkan ID Karyawan</label>
-                    <input type="text" class="form-control" id="idKaryawan" placeholder="ID Karyawan" required autofocus>
+                    <label for="cabangapotek">Pilih Cabang Apotek</label>
+                    <select id="cabangapotek" class="form-control" required>
+                    @foreach($cabang as $cab)
+                        <option value="{{$cab->id_cabang}}">{{$cab->nama_cabang}}</option>
+                    @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="idKaryawan">Pilih ID Karyawan</label>
+                    <select id="idkaryawan" class="form-control" required>
+                    @foreach($cabang as $cab)
+                        <option value="{{$cab->id_cabang}}">{{$cab->nama_cabang}}</option>
+                    @endforeach
+                    </select>
                 </div>
                 <button type="submit" class="btn btn-success">Absen</button>
             </form>
-            <!--
-            <a class= href="{{ route('home') }}">
-            <input type="submit" class="btn btn-primary" value="Kembali"></a>
-            -->
         </div>
         <div class="col">
         <!-- Isi Kolom Ketiga -->
