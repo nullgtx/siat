677e34d3844daa820345d3909b282915653ff5f0
yankotinuj <yankotinu.23@gmail.com>
2019-06-04 16:08:08 +0700

    Fix method index dan tambah method simpan

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -7,12 +7,24 @@ use App\Http\Controllers\Controller;
 use App\cabang;
 use App\Pegawais;
 use App\Absensi;
+use Auth;
 
 class AbsensiController extends Controller
 {
     public function index(){
-        $cabang = cabang::all();
-        //$cabangTerpilih = Input::get('cabangapotek');
-        return view('absensikaryawan', compact('cabang'));
+        $cabang = cabang::where('id_cabang', Auth::user()->id_cabang)->first();
+        $datapegawai = Pegawais::where('id_cabang', $cabang->id_cabang)->get();
+        return view('absensikaryawan', ['cabang' => $cabang, 'datapegawai'=> $datapegawai]);
+    }
+    public function store(Request $request) {
+        $absen = new Absensi;
+        $absen->id_cabang = $request->id_cabang;
+        $absen->idkaryawan = $request->idkaryawan;
+        $absen->tanggal = $request->tanggal;
+        $absen->keterangan = $request->keterangan;
+        $absen->save();
+        return Response::json([
+            'action' => 'save_absensi'
+                ], 201);
     }
 }
