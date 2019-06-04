b840d054c4fa323e6e5ff3df1c098aafeaa29cf5
yankotinuj <yankotinu.23@gmail.com>
2019-06-04 16:07:52 +0700

    Fix model Absensi

--- app/Absensi.php
+++ app/Absensi.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Absensi extends Model
 {
     protected $table = 'absensis';
-    protected $fillable = ['id','idkaryawan','tanggal','keterangan'];
+    protected $fillable = ['id','id_cabang','idkaryawan','tanggal','keterangan'];
     public function datapegawai(){
-        return $this->hasOne('App\Pegawais','idkaryawan','idkaryawan');
+        return $this->hasOne('App\cabang','id_cabang','id_cabang');
     }
 }
