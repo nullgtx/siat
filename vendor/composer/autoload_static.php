7d641bb47dc8808726b4e08a1dfc839c9d3506a8
yankotinuj <yankotinu.23@gmail.com>
2019-06-08 22:01:39 +0700

    up

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -360,8 +360,10 @@ class ComposerStaticInit4af4a5d5e02d71aa876d11649d874f9f
     );
 
     public static $classMap = array (
+        'App\\Absensi' => __DIR__ . '/../..' . '/app/Absensi.php',
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
+        'App\\Http\\Controllers\\AbsensiController' => __DIR__ . '/../..' . '/app/Http/Controllers/AbsensiController.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
         'App\\Http\\Controllers\\Auth\\LoginController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/LoginController.php',
         'App\\Http\\Controllers\\Auth\\RegisterController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/RegisterController.php',
