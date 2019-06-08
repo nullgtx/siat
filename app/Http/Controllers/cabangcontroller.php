0bdf13a84a6d136dfd82495763e7878d21cd46ee
yankotinuj <yankotinu.23@gmail.com>
2019-06-09 00:40:43 +0700

    tambah fungsi buat index dashboard kepala

--- app/Http/Controllers/cabangcontroller.php
+++ app/Http/Controllers/cabangcontroller.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\cabang;
+use Auth;
 
 class cabangcontroller extends Controller
 {
-    //
+    public function indexKepala(){
+        $cabang = cabang::where('id_cabang', Auth::user()->id_cabang)->first();
+        return view('dashboardkepala', ['cabang' => $cabang]);
+    }
 }
