0ee812de536b9ce4bf2381232f2fc110657d6b56
yankotinuj <yankotinu.23@gmail.com>
2019-07-19 19:37:33 +0700

    Membuat Barang Masuk Keluar

---
+++ database/migrations/2019_07_19_122711_barangmasuk.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Barangmasuk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangmasuk', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_cabang');
+            $table->string('kodebarang');
+            $table->string('jenisbarang');
+            $table->string('keteranganbarang');
+            $table->string('satuanbarang');
+            $table->bigInteger('hargabarang');
+            $table->integer('jumlahbarang');
+            $table->date('tanggalmasuk');
+            $table->date('tanggalexpired');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
